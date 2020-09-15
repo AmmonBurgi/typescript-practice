b9db24686766e4d15519682c72f88dfb9b4396da
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-09-15 11:50:43 -0600

    app prep

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import TextField from './TextField';
+
 
 
 const App: React.FC = () => {
+  const [first, setFirst] = useState<string>('')
+  const [last, setLast] = useState<string>('')
+  const [nick, setNick] = useState<string>('')
+
   return (
     <div className="App">
-      <Counter>
-        {(count, setCount) => }
-      </Counter>
+      <input placeholder='First Name' value={first} onChange={(e) => setFirst(e.target.value)} />
+      <input placeholder='Last Name' value={last} onChange={(e) => setLast(e.target.value)} />
+      <input placeholder='Nickname' value={nick} onChange={e => setNick(e.target.value)} />
     </div>
   )
 }
